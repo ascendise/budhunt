29b195fe799ab6b906aa29aa4a0f43ecab9715ef
Ascendise <ascendise@ascendise.ch>
2026-02-10 17:15:25 +0100

    create a window

---
+++ app/src/main.rs
@@ -0,0 +1,18 @@
+use glfw::Context;
+
+fn main() {
+    let mut glfw = glfw::init(glfw::fail_on_errors).unwrap();
+    let (mut window, _) = glfw
+        .create_window(1280, 720, "Budhunt", glfw::WindowMode::Windowed)
+        .expect("Failed to create window");
+    window.make_current();
+    unsafe {
+        gl::load_with(|s| window.get_proc_address(s).unwrap() as *const _);
+        gl::ClearColor(0.75, 0.75, 0.75, 1.0);
+        while !window.should_close() {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+            window.swap_buffers();
+            glfw.poll_events();
+        }
+    }
+}
